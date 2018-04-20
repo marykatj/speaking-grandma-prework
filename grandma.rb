4f5267239edbd9f1b62e4eeedadb585034540154
Mary-Kate James <marykatj@gmail.com>
2018-04-20 18:51:03 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,13 @@
 def speak_to_grandma(phrase)
-  if phrase !== phrase.uppercase
+  if phrase = "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase !== phrase.uppercase
     return "HUH?! SPEAK UP, SONNY!"
+  else
+    return "NO, NOT SINCE 1938!"
 end
+
+
 
 
 # Write a speak_to_grandma method.
